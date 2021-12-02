6c4f5fc8659b039b8f39ea0206ff8466630e2ba2
Josue Sanchez <30679533+jsanchez2388@users.noreply.github.com>
2021-12-01 20:34:51 -0800

    Paystub model and view

--- EmployeePayroll/settings.py
+++ EmployeePayroll/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-dgy@y_2$kh4zg$wdb9nqlia_7+a)i*a(!6)3&4cs!y&wgiop%="
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
